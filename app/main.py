62d33f0786d1e965449671aa1ddfb6fcac50d605
Ahmad Ghozali <ahmadgozali4040@gmail.com>
2025-12-07 16:48:44 +0700

    fix[cors]: setting up cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 import logging
-
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
 from os import environ
 
-from .routers import auth, goods, sales, dashboard, chat
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .routers import auth, chat, dashboard, forecast, goods, sales
 
 load_dotenv()
 logger = logging.getLogger(__name__)
@@ -16,11 +16,15 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*" if environ.get("DEV_KEY") == "dev" else environ.get("FRONTEND_URL")
+        (
+            "*"
+            if environ.get("DEV_ENV") == "dev"
+            else environ.get("FRONTEND_URL", "http://localhost:3000")
+        )
     ],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
@@ -28,3 +32,4 @@ app.include_router(dashboard.router)
 app.include_router(goods.router)
 app.include_router(sales.router)
 app.include_router(chat.router)
+app.include_router(forecast.router)
